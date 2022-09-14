2f8bf26d25d628a7eeee05dabd5fb6a7b5368cac
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-14 16:25:55 -0300

    Thread  em paralelo

--- thread/src/main/java/com/threadbasic/threadParalala.java
+++ thread/src/main/java/com/threadbasic/threadParalala.java
@@ -6,18 +6,30 @@ public class threadParalala {
 
     public static void main(String[] args) throws InterruptedException {
 
-        // codigo da rotina para executar em background
-        for (int i = 0; i < 10; i++) {
+        new Thread() {
 
-            /* excecutar este envio de mesnsagem com tempo deternimado */
-            System.out.println("Executando alguma rotina de envio de emails");
+            public void run() { /* exceuta o que é definido */
+                /* rotina criada */
 
-            // sleep executa uma impressão a cada 1 segundo
-            Thread.sleep(1000); // da um tempo(intervalo) para nova impresão
-        }
+                /* codigo da rotina para executar em background */
+                for (int i = 0; i < 10; i++) {
+
+                    /* excecutar este envio de mesnsagem com tempo deternimado */
+                    System.out.println("Executando alguma rotina de envio de emails");
+
+                    // sleep executa uma impressão a cada 2 segundos
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } // da um tempo(intervalo) para nova impresão
+                }
+            };
+
+        }.start(); /* start liga a thred que fica rodando paralemente */
 
         /* processo finalizado para o usuario, e o fluxo do sistema normal */
         System.out.println("Final do código.");
-        JOptionPane.showMessageDialog(null, "Sistema continua rodando para o usuario", "Mensagem", 1);
+        JOptionPane.showMessageDialog(null, "Sistema continua rodando para o usuário", "Mensagem", 1);
     }
 }
