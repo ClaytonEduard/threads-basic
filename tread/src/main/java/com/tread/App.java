4dde205496bdea4698e2287f75b8bbe5f1daa606
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-14 14:59:46 -0300

    feat: first commit

---
+++ tread/src/main/java/com/tread/App.java
@@ -0,0 +1,13 @@
+package com.tread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
