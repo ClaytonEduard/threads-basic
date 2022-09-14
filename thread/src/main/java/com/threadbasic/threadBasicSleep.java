5a9cb361841dd66698e1c40fb8b61b2da9a11ac2
ClaytonEduard <clayton_eduard@hotmail.com>
2022-09-14 15:48:11 -0300

    Sleep

---
+++ thread/src/main/java/com/threadbasic/threadBasicSleep.java
@@ -0,0 +1,17 @@
+package com.threadbasic;
+
+public class threadBasicSleep {
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {       
+
+            /* excecutar este envio de mesnsagem com tempo deternimado */
+            System.out.println("Executando alguma rotina de envio de emails");
+
+             // sleep executa uma impressão a cada 1 segundo
+             Thread.sleep(1000); //da um tempo(intervalo) para nova impresão
+        }
+        System.out.println("Final do código.");
+    }
+
+}
